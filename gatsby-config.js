286446fad11f752f11669468d77c867f43cc0f7d
Richar A. Macedo <richarallan@DESKTOP-SLAVED1.localdomain>
2020-12-07 04:53:46 -0300

    Limpando as configurações iniciais!

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Blog 3 level Web`,
+    description: `3 Level Web blog - coding is all about love!`,
+    author: `@RicharAllan`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
