869fea88a9355db45acb7b547b67dcbe1bf0e61f
Richar A. Macedo <richarallan@DESKTOP-SLAVED1.localdomain>
2020-12-08 20:23:46 -0300

    Solved error childImageSharp of null.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,26 @@ module.exports = {
     author: `@RicharAllan`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [],
       },
     },
     `gatsby-transformer-sharp`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,10 @@ import PostItem from '../components/PostItem'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home"/>
-    <PostItem 
-      slug ="/about/"
-      background = "#cecece"
-      category = "Misc"
-      date = "07 de Dezembro de 2020"
-      timeToRead = "5"
-      title = "Diga não ao Medium: tenha sua própria plataforma"
-      description = "Algumas razões para você ter a sua própria plataforma ao invés de soluções como o Medium"
-    />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
   </Layout>
 )
 
