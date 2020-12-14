3c760c8987cee504c8afd446eae85293e662b8cf
Richar A. Macedo <richar.silvamacedo@gmail.com>
2020-12-14 01:54:17 -0300

    Finish module 12, with bugs in SEO image query

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     position: `Desenvolvedor Front End`,
     description: `3 Level Web blog - coding is all about love!`,
     author: `@RicharAllan`,
+    siteUrl: `localhost:8000`
   },
   plugins: [
     `gatsby-plugin-transition-link`,
@@ -74,17 +75,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `3 Level`,
+        short_name: `3 Level`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -46,6 +46,14 @@ exports.createPages = ({ graphql, actions }) => {
                 date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
                 description
                 title
+                image {
+                  id
+                  childImageSharp {
+                      resize {
+                          src
+                      }
+                  }
+                }
               }
               timeToRead
             }
